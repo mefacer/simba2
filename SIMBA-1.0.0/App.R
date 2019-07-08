8b275a2b27170ea1381c7a94dbc53d73c7c30f46
mefacer <48984345+mefacer@users.noreply.github.com>
2019-07-08 15:19:33 +0200

    prova

--- SIMBA-1.0.0/App.R
+++ SIMBA-1.0.0/App.R
@@ -2,7 +2,7 @@
 
 ##script.dirname <- "/home/anrodriguez/simba/SIMBA-1.0.0/"
 
-script.dirname <- "/Users/aleixrvr/Projects/Code/simba2/SIMBA-1.0.0/"
+script.dirname <- paste0(getwd(),"/")
 
 source(paste0(script.dirname,"init.R")) # Script per obrir tots els paquets necesaris
 source(paste0(script.dirname,"ui.R")) # User Interface
